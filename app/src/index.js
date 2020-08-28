9b500c2c6e69e9f103c0987b3626b771f7b640f9
Sean Felderman <sean.felderman@gmail.com>
2020-08-27 22:17:57 -0700

    frontend auth, not pretty

--- app/src/index.js
+++ app/src/index.js
@@ -4,10 +4,18 @@ import ReactDOM from 'react-dom';
 // import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import AppRouter from './AppRouter';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppRouter />
+    <Auth0Provider
+      domain='plaid-fullstack.us.auth0.com'
+      clientId='EfY45TSGV6iZeF7ThFCKh3vN2RIHsdLL'
+      redirectUri={window.location.origin}
+      audience='https://plaid-fullstack.us.auth0.com/api/v2/'
+      scope='read:current_user update:current_user_metadata'>
+      <AppRouter />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
