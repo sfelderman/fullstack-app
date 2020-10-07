44a0026cb247b0615331d2927262f01a3d4ffcf4
Sean Felderman <sean.felderman@gmail.com>
2020-10-06 21:45:05 -0700

    iso time changes

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -15,14 +15,16 @@ const resolverMap: Resolvers = {
     name: 'Date',
     description: 'Date custom scalar type',
     parseValue(value) {
-      return new Date(value); // value from the client
+      return new Date(value).toISOString(); // value from the client
     },
     serialize(value) {
-      return value.getTime(); // value sent to the client
+      return value.toISOString(); // value sent to the client
     },
     parseLiteral(ast) {
       if (ast.kind === Kind.INT) {
         return parseInt(ast.value, 10); // ast value is always in string format
+      } else if (ast.kind === Kind.STRING) {
+        return ast.value; // ast value is always in string format
       }
       return null;
     }
